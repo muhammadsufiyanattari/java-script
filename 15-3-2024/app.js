3b86d029c7d17c624df5390bed860e34b767930e
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-03-17 22:42:23 +0500

    added all folders and files

---
+++ 15-3-2024/app.js
@@ -0,0 +1,10 @@
+var aar=["audi",'volvo','ford','lamborghni']
+document.write(
+    `<h1>favorite cars</h1>
+<p>first index value is 0</p>
+<p>my first car ${aar[0]}</p>
+<p>last index value is 3</p>
+<p>my last car ${aar[3]}</p>
+
+    `
+)

b506d98ae6ed1b104039122899324dc9331298c5
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-03-17 23:23:03 +0500

    update  all

--- 15-3-2024/app.js
+++ 15-3-2024/app.js
@@ -1,10 +1,63 @@
-var aar=["audi",'volvo','ford','lamborghni']
-document.write(
-    `<h1>favorite cars</h1>
-<p>first index value is 0</p>
-<p>my first car ${aar[0]}</p>
-<p>last index value is 3</p>
-<p>my last car ${aar[3]}</p>
+// chapter-number-14-to-16
+// Q1
+// var studentName=['asmat','ali'];
+// document.write(
+//     `
+//     <h1>student name<br> ${studentName[3]}</h1>
 
-    `
-)
+    
+    
+//     `
+// );
+// Q2
+// var arr=[];
+// document.write(
+// `<h2>Student Name<br> ${arr[0]}</h2>
+
+
+
+// `
+// );
+
+// Q3
+// var arr=['hello','hay','how are you'];
+// console.log(
+// arr
+
+
+// );
+
+// Q4
+
+// var arr=[12,34,4,65];
+// console.log(
+//     arr
+// );
+
+// Q5
+//  var arr=[true,false];
+//  console.log(
+//     arr
+//  );
+
+
+// Q6
+// var arr=[12,true,'my name is sufiyan',false];
+// console.log(
+//     arr
+// );
+
+// Q7
+
+
+
+// var aar=["audi",'volvo','ford','lamborghni']
+// document.write(
+//     `<h1>favorite cars</h1>
+// <p>first index value is 0</p>
+// <p>my first car ${aar[0]}</p>
+// <p>last index value is 3</p>
+// <p>my last car ${aar[3]}</p>
+
+//     `
+// )

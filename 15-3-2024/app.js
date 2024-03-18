57d150071d13c1ae7e518653455d872c6e270e16
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-03-18 23:30:40 +0500

    update

--- 15-3-2024/app.js
+++ 15-3-2024/app.js
@@ -48,6 +48,67 @@
 // );
 
 // Q7
+// var aar=['SSC', 'HSC', 'BCS','BS', 'BCOM', 'MS', 'M.Phil', 'PhD']
+// document.write(
+//     `<h1>QUALIFICATIONS IN PAKISTAN</h1>
+//    1)${ aar[0]}<br>
+//    2)${ aar[1]}<br>
+//    3)${ aar[2]}<br>
+//    4)${ aar[3]}<br>
+//    5)${ aar[4]}<br>
+//    6)${ aar[5]}<br>
+//    7)${ aar[6]}<br>
+//    8)${ aar[7]}
+// `
+// );
+
+// Q8
+
+// var student=['kabeer','ahemad','rafey'];
+// var total=[320/500*100,230/500*100,480/500*100]
+// document.write(
+//     `
+//    Sorce of ${student[0]} 320 percentage ${total[0]}% <br>
+//      Sorce of ${student[1]} 320 percentage ${total[1]}% <br>
+//       Sorce of ${student[2]} 320 percentage ${total[2]}% <br>
+
+//     `
+// )
+// Q10
+// var score=[320,230,480,120]
+// document.write(
+//     `Score of student:${score}<br>`
+// )
+
+// document.write(
+//      `Order Score of student: ${score.sort()} `
+// )
+
+// Q11
+// var citiesName=['karachi','lahore','islamabad','peshawar'];
+// document.write(citiesName +'<br>');
+// var citiesName2=citiesName.slice(1,3);
+// document.write(
+//     `Select city list:<br>
+    
+//     ${
+//         citiesName2
+//     }
+//     `
+// )
+
+// Q12
+// var arr=['This','is','my','cat'];
+// document.write("<h1>Array</h1>"+arr+"<br>"
+// +'<h1>String</h1>'+arr[0]+' '+arr[1]+' '+arr[2]+' '+arr[3]
+
+// );
+
+
+
+
+
+
 
 
 
